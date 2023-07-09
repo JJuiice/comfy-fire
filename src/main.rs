9eecea130d67a98dec0e069dc0459a2f724275d0
Ojas Anand <oanand@fastmail.com>
2023-07-09 07:13:24 +0000

    Got basic sdl tutorial working with module separation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod vid;
 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 use std::time::Duration;
 
 pub fn main() {
+    let mut sdl2sys = vid::Sdl2Sys::new();
+
     // let ctx = sdl2::init().unwrap();
     // let vid_subsys = ctx.video().unwrap();
 
@@ -13,28 +19,26 @@ pub fn main() {
     // canvas.set_draw_color(Color::RGB(0, 255, 255));
     // canvas.clear();
     // canvas.present();
-    // let mut evt_pump = ctx.event_pump().unwrap();
-    // let mut i = 0;
-    // 'running: loop {
-    //     i = (i + 1) % 255;
-    //     canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-    //     canvas.clear();
-    //     for evt in evt_pump.poll_iter() {
-    //         match evt {
-    //             Event::Quit {..} |
-    //             Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-    //                 break 'running
-    //             },
-    //             _ => {}
-    //         }
-    //     }
+    let mut evt_pump = sdl2sys.get_evt_pump();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        sdl2sys.set_draw_color(Color::RGB(i, 64, 255 - i));
+        // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        // canvas.clear();
+        for evt in evt_pump.poll_iter() {
+            match evt {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
 
-    //     canvas.present();
-    //     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    // }
+        sdl2sys.show_draw_color();
+        // canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 
-    vid::Sdl2Sys::new();
-
-    // ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32));
 }
--- src/vid.rs
+++ src/vid.rs
@@ -1,11 +1,9 @@
 extern crate sdl2;
 
 use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
 
 pub struct Sdl2Sys {
-	// ctx: sdl2::Sdl,
+	ctx: sdl2::Sdl,
 	// vid: sdl2::VideoSubsystem,
 	// win: sdl2::video::Window,
 	canvas: sdl2::render::WindowCanvas
@@ -17,15 +15,28 @@ impl Sdl2Sys {
 		let vid = ctx.video().unwrap();
 		let win = vid.window("Comfy Fire", 800, 600).position_centered().build().unwrap();
 		let mut canvas = win.into_canvas().build().unwrap();
-		canvas.set_draw_color(Color::RGB(0, 0, 0));
+		canvas.set_draw_color(Color::RGB(0, 255, 255));
 		canvas.clear();
 		canvas.present();
 
 		Sdl2Sys {
-			// ctx: ctx,
+			ctx: ctx,
 			// vid: vid,
 			// win: win,
 			canvas: canvas
 		}
+	}
+
+	pub fn set_draw_color(&mut self, color: sdl2::pixels::Color) {
+		self.canvas.set_draw_color(color);
+		self.canvas.clear();
+	}
+
+	pub fn show_draw_color(&mut self) {
+		self.canvas.present();
+	}
+
+	pub fn get_evt_pump(&self) -> sdl2::EventPump {
+		self.ctx.event_pump().unwrap()
 	}
 }
