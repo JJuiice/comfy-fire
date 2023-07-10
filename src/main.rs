67a45ebb788bdd51cb0feca35a989eb0e078bd62
Ojas Anand <oanand@fastmail.com>
2023-07-10 05:34:23 +0000

    Implemented drawing points for fire along bottom of screen

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,9 @@ use std::time::Duration;
 pub fn main() {
     let mut sdl2sys = vid::Sdl2Sys::new();
 
-    // let ctx = sdl2::init().unwrap();
-    // let vid_subsys = ctx.video().unwrap();
-
-    // let win = vid_subsys.window("Comfy Fire", 800, 600).position_centered().build().unwrap();
-
-    // let mut canvas = win.into_canvas().build().unwrap();
-
-    // canvas.set_draw_color(Color::RGB(0, 255, 255));
-    // canvas.clear();
-    // canvas.present();
+    sdl2sys.set_draw_color(Color::RGB(255, 255, 0));
     let mut evt_pump = sdl2sys.get_evt_pump();
-    let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        sdl2sys.set_draw_color(Color::RGB(i, 64, 255 - i));
-        // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        // canvas.clear();
         for evt in evt_pump.poll_iter() {
             match evt {
                 Event::Quit {..} |
@@ -37,7 +23,6 @@ pub fn main() {
         }
 
         sdl2sys.show_draw_color();
-        // canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
--- src/vid.rs
+++ src/vid.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 
 use sdl2::pixels::Color;
+use sdl2::rect::Point;
 
 pub struct Sdl2Sys {
 	ctx: sdl2::Sdl,
@@ -15,7 +16,7 @@ impl Sdl2Sys {
 		let vid = ctx.video().unwrap();
 		let win = vid.window("Comfy Fire", 800, 600).position_centered().build().unwrap();
 		let mut canvas = win.into_canvas().build().unwrap();
-		canvas.set_draw_color(Color::RGB(0, 255, 255));
+		canvas.set_draw_color(Color::RGB(0, 0, 0));
 		canvas.clear();
 		canvas.present();
 
@@ -29,7 +30,18 @@ impl Sdl2Sys {
 
 	pub fn set_draw_color(&mut self, color: sdl2::pixels::Color) {
 		self.canvas.set_draw_color(color);
-		self.canvas.clear();
+		// self.canvas.clear();
+		let mut i = 0;
+		while i < self.canvas.window().size().0.try_into().unwrap() {
+			// let _ = self.canvas.draw_point(Point::new(i, self.canvas.window().size().1.try_into().unwrap()));
+			let _ = self.canvas.draw_point(Point::new(i, 599));
+			i = i + 3;
+		}
+		// let ps: [Point; self.canvas.window().size().0.try_into().unwrap() / 2];
+		// let ps = (0..self.canvas.window().size().0.try_into().unwrap()).filter(|x| x % 2 == 0)
+		// let p1 = Point::new(0, 100);
+		// let p2 = Point::new(self.canvas.window().size().0.try_into().unwrap(), 100);
+		// let _ = self.canvas.draw_line(p1, p2);
 	}
 
 	pub fn show_draw_color(&mut self) {
